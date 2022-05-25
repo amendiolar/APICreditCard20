4030b2116a58cd4d23b2f4684870972f19d82768
Abel Mendiola <abel.mendiola@comunidad.unam.mx>
2022-05-24 19:58:51 -0500

    Resilience4j incluido

--- src/main/java/com/ibm/creditcard/controllers/CreditCardController.java
+++ src/main/java/com/ibm/creditcard/controllers/CreditCardController.java
@@ -3,17 +3,22 @@ package com.ibm.creditcard.controllers;
 import com.ibm.creditcard.model.entities.CreditCard;
 import com.ibm.creditcard.services.CreditCardDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/creditCard")
 public class CreditCardController
 {
+    @Autowired
+    private CircuitBreakerFactory circuitBreaker;
+
     @Autowired
     private CreditCardDAO creditCardDao;
 
@@ -29,8 +34,39 @@ public class CreditCardController
     @GetMapping("/sugerencia")
     public ResponseEntity<?> buscarTarjetaSugerida(@RequestParam(name="passion") String yourPassion, @RequestParam(name="salary") Double monthlySalary, @RequestParam Integer age)
     {
-        Iterable<CreditCard> creditCard = creditCardDao.buscarTarjetaSugerida(yourPassion, monthlySalary,age);
-        return new ResponseEntity<Iterable<CreditCard>>(creditCard, HttpStatus.OK);
+        return circuitBreaker.create("tarjetas")
+                .run(()->{
+                    Iterable<CreditCard> creditCard = creditCardDao.buscarTarjetaSugerida(yourPassion, monthlySalary,age);
+                    if(true){
+                        throw new RuntimeException("Fallo a la peticion");
+                    }
+                    return new ResponseEntity<Iterable<CreditCard>>(creditCard, HttpStatus.OK);
+                }, e-> metodoAlternativo(yourPassion,monthlySalary,age,e));
+
     }
 
+    public ResponseEntity<?> metodoAlternativo(String yourPassion, Double monthlySalary, Integer age, Throwable e)
+    {
+        HashMap<String, String> sugerencia = new HashMap<String, String>();
+
+        sugerencia.put("passion",yourPassion);
+        sugerencia.put("monthlySalary",monthlySalary.toString());
+        sugerencia.put("age",age.toString());
+
+        return new ResponseEntity<>(sugerencia,HttpStatus.REQUEST_TIMEOUT);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
